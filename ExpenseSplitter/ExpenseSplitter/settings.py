0be32f51f54586a1901e45ccf126f494530882ad
wkostusiak <wkostusiak@gmail.com>
2022-12-23 20:37:36 +0100

    final corrections

--- ExpenseSplitter/ExpenseSplitter/settings.py
+++ ExpenseSplitter/ExpenseSplitter/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
--- ExpenseSplitter/expense_list/models.py
+++ ExpenseSplitter/expense_list/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.conf import settings
 
 
 class Category(models.Model):
